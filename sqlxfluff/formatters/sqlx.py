9cc60bbc8e126040b8b3589c088e06fe0297c2fd
James Cottrill <james@slatedata.co.uk>
2024-04-04 11:28:31 +0100

    feat: fix and return pre and post operations

--- sqlxfluff/formatters/sqlx.py
+++ sqlxfluff/formatters/sqlx.py
@@ -11,6 +11,15 @@ def format_sqlx(deconstructed_file: dict, config: FluffConfig):
     bq_fix_result = sqlfluff.fix(deconstructed_file["main"], config=config).rstrip(
         ";\n"
     )
+    pre_operations_fix, post_operations_fix = None, None
+    if deconstructed_file["pre_operations"]:
+        pre_operations_fix = sqlfluff.fix(deconstructed_file["pre_operations"], config=config).rstrip(
+            ";\n"
+        )
+    if deconstructed_file["post_operations"]:
+        post_operations_fix = sqlfluff.fix(deconstructed_file["post_operations"], config=config).rstrip(
+            ";\n"
+        )
     # place the templates back into the SQLX
     for mask, template in deconstructed_file["templates"].items():
         formatted_template = format_template(template)
@@ -20,4 +29,14 @@ def format_sqlx(deconstructed_file: dict, config: FluffConfig):
 
     # recombine the config block and the fixed SQL
     formatted_config_block = format_config(deconstructed_file["config"])
-    return formatted_config_block + "\n\n" + bq_fix_result + "\n"
+    js_block = deconstructed_file["js"]
+    formatted_file = f"{formatted_config_block}\n\n"
+    if js_block:
+        formatted_file += f"{js_block}\n\n"
+    if pre_operations_fix:
+        formatted_file += f"{pre_operations_fix}\n\n"
+    if post_operations_fix:
+        formatted_file += f"{post_operations_fix}\n\n"
+    formatted_file += f"{bq_fix_result}\n"
+    
+    return formatted_file
